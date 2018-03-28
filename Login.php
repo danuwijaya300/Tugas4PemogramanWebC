396193ae219378f95d84ce8ab7d0382da85a80c3
danuwijaya300 <33424901+danuwijaya300@users.noreply.github.com>
2018-03-28 22:49:45 +0700

    Add files via upload

---
+++ Login.php
@@ -0,0 +1,80 @@
+<?php
+
+session_start();            
+function login(){
+    echo '<table>
+        
+		<form method="post" action="">
+            <tr>
+                <td> Username</td><td><input type="text" name="user"></td>
+            </tr>
+            <tr>
+                <td>Password</td><td><input type="text" name="pass"></td>   
+            </tr>
+            <tr>
+                <td></td><td><input type="submit" name="submit" value="Login"></td>
+            </tr>
+        </form>
+    </table>';
+}
+
+
+
+
+
+//jika tombol login diklik
+if(isset($_POST['submit']))
+{
+        if($_POST['user']=="Admin" && $_POST['pass']=="Admin")
+        {
+            // disini code ketika login berhasil
+            // abaikan saja karna pembahasan kali ini adalah ketika login gagal  
+        }
+        else
+        {
+            //jika terdapat session bernama "auth"
+            //untuk penamaan session bebas 
+            if(isset($_SESSION['auth']))
+            {
+                //jika user gagal masuk selama 3 kali atau lebih
+                if($_SESSION['auth']>3 || $_SESSION['auth']==3){
+                        //set nilai session "auth" ke 4
+                        $_SESSION['auth']=4;
+                        //jalankan function blokir_user
+                        blokir_user();
+                }
+                //jika tidak
+                else{
+                        //session "auth" ditambah 1
+                        $_SESSION['auth']=$_SESSION['auth']+1;
+                        //jalankan function login()
+                        login();
+                }
+            }
+            //jika tidak ada session "auth"
+            else{
+                    //daftarkan session "auth", dan beri nilai 1
+                    $_SESSION['auth']=1;
+                    //jalankan function login()
+                    login();
+            }
+ 
+        }
+}
+//jika tombol login tidak diklik ya tampilkan form loginnya
+else
+{
+        //jalankan function login()
+        login();
+}
+function blokir_user(){
+    echo "<h1> Anda telah diblokir, Silahkan Hubungi Operator Anda ! </h1>";
+}
+ 
+//ini script untuk menampilkan brapa kali user mencoba login
+if(isset($_SESSION['auth'])){
+    echo $_SESSION['auth'];
+}
+
+
+?>
